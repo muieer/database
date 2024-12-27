898fdac11bf1f10e982ab964b0b4780e0cfea0b8
muieer <yang_for_work@qq.com>
2024-12-27 15:31:14 +0800

    init

---
+++ sql/doris.sql
@@ -0,0 +1,42 @@
+
+select * from user_order order by cost desc limit 1,10;
+
+describe user_order;
+
+CREATE TABLE IF NOT EXISTS user_order
+(
+    uid  INT COMMENT '用户 ID',
+    cost DECIMAL(10, 2) DEFAULT '0.0',
+    dt   datetime  DEFAULT current_timestamp
+)
+    ENGINE=olap
+    DISTRIBUTED BY HASH(uid) BUCKETS 8
+PROPERTIES (
+    "replication_num" = "1"
+);
+
+describe user;
+
+show tables;
+
+CREATE TABLE user
+(
+    uid int
+)
+    COMMENT "my first table"
+    DISTRIBUTED BY HASH
+(
+    uid
+) BUCKETS 32
+    PROPERTIES
+(
+    "replication_num" = "1"
+);
+
+use learn;
+
+show databases;
+
+create database learn;
+
+
